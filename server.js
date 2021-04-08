50c89507319f1a062082b27849bd31fc785c00b2
Scott Steinbeck <uniquetrio2000@gmail.com>
2021-04-08 12:12:13 -0700

    show port running message

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ var httpProxy = require('http-proxy');
 var port = process.env.PORT || 3000;
 
 //create our proxy to the new websocket connection
-httpProxy.createProxyServer({ target: 'http://agritrackingsystems.com:3000' }).listen(port);
+httpProxy.createProxyServer({ target: 'http://agritrackingsystems.com:3000' })
+          .listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
