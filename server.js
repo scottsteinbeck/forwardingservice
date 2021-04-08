2575cb3e39acfb1f75033fc3ced1dfe271820b94
Scott Steinbeck <uniquetrio2000@gmail.com>
2021-04-08 11:49:31 -0700

    update comments

--- server.js
+++ server.js
@@ -1,2 +1,4 @@
 var httpProxy = require('http-proxy');
+
+//create our proxy to the new websocket connection
 httpProxy.createProxyServer({target:'http://agritrackingsystems.com:3000'}).listen(8080); // See (†)
