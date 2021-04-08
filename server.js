93165209968dc875a5604ddc47c5a80ff1b4682c
Scott Steinbeck <uniquetrio2000@gmail.com>
2021-04-08 11:56:25 -0700

    update settings

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var httpProxy = require('http-proxy');
+var port = process.env.PORT || 3000;
 
 //create our proxy to the new websocket connection
-httpProxy.createProxyServer({target:'http://agritrackingsystems.com:3000'}).listen(80); // See (†)
+httpProxy.createProxyServer({ target: 'http://agritrackingsystems.com:3000' }).listen(port);
